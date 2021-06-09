aba1d57d441f332595b5c04c94b007a5afbedee7
wesley-howell <wesley.howell@kellogg.northwestern.edu>
2021-06-09 16:35:05 -0500

    x

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyBc63N7CVW6dxHsNFfgt8NNo0b59WYNVrs",
+  authDomain: "kiei451w7class.firebaseapp.com",
+  projectId: "kiei451w7class",
+  storageBucket: "kiei451w7class.appspot.com",
+  messagingSenderId: "1092826794203",
+  appId: "1:1092826794203:web:6fc355408b6569560ba4fe"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
